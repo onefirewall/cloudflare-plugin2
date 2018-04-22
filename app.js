cddcc5b6ac90a3510d7efcbd1a560fd647e8b316
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-23 01:35:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var CloudFlareOFA = require('./CloudFlareOFA.js'),
     api_url_zones = "https://api.cloudflare.com/client/v4/zones?page=1&per_page=1000&order=type&direction=asc",
     api_url = "https://api.cloudflare.com/client/v4/zones/",
-    api_url_post_delete = "https://api.cloudflare.com/client/v4/user/firewall/access_rules/rules",
     x_auth_key = "globalkey",
     x_auth_email = "email",
     ip = "3.3.3.3",
     mode = "block",
     id = "id_to_delete_IP",
+    zoneId = "zoneId",
     addBlockIp = {"mode": mode, "configuration": {"target": "ip", "value": ip}, "notes": "Test OFA"},
-    cloudflareOFA = new CloudFlareOFA(api_url_zones, api_url, api_url_post_delete, x_auth_key, x_auth_email, addBlockIp, id);
+    cloudflareOFA = new CloudFlareOFA(api_url_zones, api_url, x_auth_key, x_auth_email, addBlockIp, id, zoneId);
 
 cloudflareOFA.getAllBlockedIPS(
    function callback (data) {
@@ -21,6 +21,6 @@ cloudflareOFA.setAllBlockedIPS(
 });
 
 cloudflareOFA.deleteIPS(
-   function callback (response) {
-       console.log(JSON.stringify(response));
+    function callback (response) {
+        console.log(JSON.stringify(response));
 });
