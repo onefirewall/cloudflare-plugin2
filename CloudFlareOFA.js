2e5ea3bd586c4e249231622b35eb3605fdfd5135
OneFirewall <37590172+onefirewall@users.noreply.github.com>
2018-04-23 14:02:33 +0200

    Update CloudFlareOFA.js

--- CloudFlareOFA.js
+++ CloudFlareOFA.js
@@ -8,11 +8,13 @@ var CloudFlareOFA = function (api_url_zones, api_url, x_auth_key, x_auth_email, 
             'X-Auth-Key': x_auth_key,
             'Content-Type': 'application/json'
         },
+
         optionsGetZones = {
             url: api_url_zones,
             method: 'GET',
             headers: headers
         },
+
         optionsPOST = {
             url: api_url + zoneId + '/firewall/access_rules/rules',
             method: 'POST',
@@ -30,7 +32,7 @@ var CloudFlareOFA = function (api_url_zones, api_url, x_auth_key, x_auth_email, 
     	request(optionsGetZones,
             function (error, response, body) {
                 if (error || response.statusCode !== 200) {
-                    return callback(error || body);
+                    callback(error || body);
                 }
                 createMyZones(body, api_url, headers, callback);
             });
@@ -40,7 +42,7 @@ var CloudFlareOFA = function (api_url_zones, api_url, x_auth_key, x_auth_email, 
         request(optionsPOST,
             function (error, response, body) {
                 if (error || response.statusCode !== 200) {
-                    return callback(error);
+                    callback(error);
                 }
                 callback(response.statusCode);
             });
@@ -50,7 +52,7 @@ var CloudFlareOFA = function (api_url_zones, api_url, x_auth_key, x_auth_email, 
         request(optionsDELETE,
             function (error, response, body) {
                 if (error || response.statusCode !== 200) {
-                    return callback(error);
+                    callback(error);
                 }
                 callback(response.statusCode);
             });
@@ -61,7 +63,7 @@ function createMyZones(validElements, api_url, headers, callback) {
     var resp = JSON.parse(validElements),
         result = resp.result,
         dataIndex = 0;
-    
+
     _.map(result, function (item) {
     	dataIndex++;
     	createGetRest(result.length, item.id, api_url, headers, dataIndex, callback);
@@ -70,21 +72,24 @@ function createMyZones(validElements, api_url, headers, callback) {
 
 function createGetRest(size, zoneId, api_url, headers, dataIndex, callback) {
     var respData,
+        data = [],
         optionsGET = {
             url: api_url + zoneId + '/firewall/access_rules/rules?page=1&per_page=1000&order=type&direction=asc',
             method: 'GET',
             headers: headers
         };
+
     request(optionsGET,
         function (error, response, body) {
             if (error || response.statusCode !== 200) {
                 return callback(error || body);
             }
-        if (dataIndex == size) {
-            callback(createMyJson(body, zoneId));
-        } else {
-            createMyJson(body, zoneId)
-        }
+
+            data.push(createMyJson(body, zoneId));
+
+            if (dataIndex == size) {
+                callback(data);
+            }
     });
 };
 
